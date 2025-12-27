35d483ee2097f5c7645d9afc3608627e73074b7b
tree_division <qbu666666@qq.com>
2025-12-27 21:16:05 +0800

    Add background

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from blogs.views import register  # 新增导入
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 认证相关
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/register/', register, name='register'),  # 新增注册路由
+    
     path('', include('blogs.urls')),
 ]
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.urls import reverse_lazy
 from .models import BlogPost
 from .forms import BlogPostForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 class HomePageView(ListView):
     model = BlogPost
